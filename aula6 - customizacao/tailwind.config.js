a3ae9683925d7f377662f0aca6cafe5a5fc7322c
Lucas <lucasbonfim604@gmail.com>
2023-10-02 20:08:11 -0300

    customizacao

---
+++ aula6 - customizacao/tailwind.config.js
@@ -0,0 +1,21 @@
+
+/** @type {import('tailwindcss').Config} */
+const  colors  = require('tailwindcss/colors');
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'new-blue': '#243C5A',
+        gray:{
+          ...colors.gray,
+          '900': '#999'
+        },
+      },
+      spacing:{
+        '50': '20rem'
+      }
+    },
+  },
+  plugins: [],
+}
